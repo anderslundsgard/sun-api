50708863cf99b7c518a615a59765b2e33a2a32c9
Anders Lundsgård <anders@utvecklare.eu>
2015-12-31 11:12:42 +0100

    Started to add OWIN to enable tests.

---
+++ SunApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+
+[assembly: OwinStartup(typeof(SunApi.Startup))]
+
+namespace SunApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            var configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            // Execute any other ASP.NET Web API-related initialization, i.e. IoC, authentication, logging, mapping, DB, etc.
+            //ConfigureAuthPipeline(app);
+            //app.UseWebApi(configuration);
+        }
+    }
+}
---
+++ SunTests/SystemTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SunTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    using SunApi;
+
+    [TestFixture]
+    public class SystemTests
+    {
+        //private static IDisposable _webApp;
+
+        //[SetUp]
+        //public static void SetUp(TestContext context)
+        //{
+        //    _webApp = WebApp.Start<Startup>("http://*:9443/");
+        //}
+
+        //[TearDown]
+        //public static void TearDown()
+        //{
+        //    _webApp.Dispose();
+        //}
+
+        //[Test]
+        //public async Task TestMethod()
+        //{
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        var accessToken = GetAccessToken();
+        //        httpClient.DefaultRequestHeaders.Authorization =
+        //            new AuthenticationHeaderValue("Bearer", accessToken);
+        //        var requestUri = new Uri("http://localhost:9443/api/values");
+        //        await httpClient.GetStringAsync(requestUri);
+        //    }
+        //}
+    }
+}
