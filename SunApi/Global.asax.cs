6018cbb9cd5a2fd35ba36508d9181b8ff1bd5f95
Anders Lundsgård <anders@utvecklare.eu>
2016-01-01 12:09:42 +0100

    Cleaning; Bundle.

--- SunApi/Global.asax.cs
+++ SunApi/Global.asax.cs
@@ -17,7 +17,6 @@ namespace SunApi
             GlobalConfiguration.Configure(WebApiConfig.Register);
             //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
