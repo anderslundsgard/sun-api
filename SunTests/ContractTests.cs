4377e5c4b279e2483f0138b6bab0fad700ef1f58
Anders Lundsgård <anders@utvecklare.eu>
2016-01-02 16:10:43 +0100

    Added a first dummy contract test.
    Using Owin.Testing

---
+++ SunTests/ContractTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; 
+
+namespace SunTests
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    using Microsoft.Owin.Hosting;
+    using Microsoft.Owin.Testing;
+
+    using Owin;
+
+    using SunApi;
+
+    [TestFixture]
+    public class ContractTests
+    {
+        [Test]
+        public void OwinAppTest()
+        {
+            using (var server = TestServer.Create<MyStartup>())
+            {
+                var response = server.HttpClient.GetAsync("/").Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                Assert.IsTrue(content.Contains("Hello world using OWIN TestServer"));
+            }
+        }
+    }
+
+    public class MyStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWelcomePage().UseErrorPage(); // See Microsoft.Owin.Diagnostics
+            //app.Us("/Welcome"); // See Microsoft.Owin.Diagnostics 
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello world using OWIN TestServer");
+            });
+        }
+    }
+}
