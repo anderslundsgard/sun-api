c51bd84df21e9715a305de8cdcf6652d78e6e099
Anders Lundsgård <anders@utvecklare.eu>
2016-01-04 06:01:06 +0100

    First real working Contract tests.

--- SunTests/ContractTests.cs
+++ SunTests/ContractTests.cs
@@ -8,6 +8,7 @@ using NUnit.Framework;
 namespace SunTests
 {
     using System.Diagnostics.CodeAnalysis;
+    using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
 
@@ -23,13 +24,13 @@ namespace SunTests
     {
         TestServer _server;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void SetUp()
         {
             this._server = TestServer.Create<Startup>();
         }
 
-        [TearDown]
+        [OneTimeTearDown]
         public void TearDown()
         {
             this._server.Dispose();
@@ -40,14 +41,35 @@ namespace SunTests
         {
             // Arrange
             var uri = "/api/sunrise";
-            var expectedresult = "[\"Refer\",\"to\",\"Swagger\",\"documentation\"]";
-            
+            var expectedxmlresult = "<ArrayOfString xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><string>Refer</string><string>to</string><string>Swagger</string><string>documentation</string></ArrayOfString>";
+
             // Act
-            var response = this._server.HttpClient.GetAsync(uri).Result;
-            var content = response.Content.ReadAsStringAsync().Result;
+            var returnedXml = this.GetXmlRequest(uri);
 
             // Assert
-            Assert.AreEqual(content, expectedresult);
+            Assert.AreEqual(returnedXml, expectedxmlresult);
+        }
+
+        [Test]
+        public void Verify_ApiSunrise_2015_11_30()
+        {
+            // Arrange
+            var uri = "/api/sunrise?lat=59.76&lon=17.13&date=2015-11-30";
+            var expectedxmlresult = "<astrodata xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><time date=\"2015-11-30T00:00:00Z\"><location latitude=\"59.76\" longitude=\"17.13\"><sun rise=\"2015-11-30T07:21:34Z\" set=\"2015-11-30T13:57:57Z\"><noon altitude=\"8.617\" /></sun><moon phase=\"Third quarter\" rise=\"2015-11-30T19:18:10Z\" set=\"2015-11-30T10:30:30Z\" /></location></time></astrodata>";
+
+            // Act
+            var returnedXml = this.GetXmlRequest(uri);
+
+            // Assert
+            Assert.AreEqual(returnedXml, expectedxmlresult);
+        }
+
+        private string GetXmlRequest(string uri)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Add(HttpRequestHeader.Accept.ToString(), "application/xml");
+            var returnedXml = this._server.HttpClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+            return returnedXml;
         }
     }
 
