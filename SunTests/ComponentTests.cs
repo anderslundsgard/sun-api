7ee3172deb5b207e3813663ab5c68016c1716011
Anders Lundsgård <anders@utvecklare.eu>
2016-01-11 22:21:59 +0100

    Added CalcSunTimes by Zacky Pickholz
    First version with a comparison test against api.yr.no. Made
    CalcSunTimes to work with only UTC.

--- SunTests/ComponentTests.cs
+++ SunTests/ComponentTests.cs
@@ -1,4 +1,6 @@
-﻿namespace SunTests
+﻿using System.Diagnostics;
+
+namespace SunTests
 {
     using System;
     using System.IO;
@@ -55,6 +57,33 @@
 
             // Assert
             Assert.IsTrue(ass.Contains("moon phase=\"Unknown\" rise=\"2015-12-09T14:00:00\" set=\"2015-12-09T15:00:00\""));
+        }
+
+        [Test]
+        public void Verify_CalcSunTimes_By_Zacky_Pickholz()
+        {
+            DateTime date = new DateTime(2016, 1, 10);
+            bool isSunrise = false;
+            bool isSunset = false;
+            DateTime sunrise = DateTime.Now;
+            DateTime sunset = DateTime.Now;
+
+            SunTimes.Instance.CalculateSunRiseSetTimes(
+                59.21318747,
+                17.41151598,
+                date, 
+                ref sunrise, 
+                ref sunset,
+                ref isSunrise, 
+                ref isSunset);
+
+            Debug.Print(date + ": Sunrise @" + sunrise.ToString("HH:mm") + "Sunset @" + sunset.ToString("HH:mm"));
+
+            Assert.AreEqual(sunrise.Hour, 7);
+            Assert.AreEqual(sunrise.Minute, 39);
+            Assert.AreEqual(sunset.Hour, 14);
+            Assert.AreEqual(sunset.Minute, 17);
+
         }
 
         private static Astrodata CreateSampleAstrodata()
--- SunTests/IntegrationTests.cs
+++ SunTests/IntegrationTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Remoting.Metadata.W3cXsd2001;
+﻿using System.Diagnostics;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Threading;
 
 namespace SunTests
 {
@@ -7,6 +9,10 @@ namespace SunTests
     using SunLib.Adapters;
     using SunLib.Models;
     using SunLib.Utils;
+
+    // Latitude : max/min +90 to -90
+    // Longitude : max/min +180 to -180
+
 
     [TestFixture]
     public class IntegrationTests
@@ -32,6 +38,72 @@ namespace SunTests
             Astrodata data = parser.GetAstrodataByResult(result);
 
             Assert.AreEqual(data.Data.Location.Sun.Rise, new DateTime(2015, 11, 26, 07, 06, 35));
+        }
+
+        [Test]
+        public void Compare_YrNo_And_Zacky_Pickholz_Algorithm()
+        {
+            IYrNoAdapter adapter = new YrNoAdapter();
+
+            for (int i = 0; i < 10; i++)
+            {
+                double lat = this.GetRandLat();
+                double lon = this.GetRandLong();
+                DateTime date = new DateTime(2015, 09, 23);
+
+                try
+                {
+                    var resultYr = adapter.GetSunInfo(lat, lon, date);
+                    IYrNoResultParser parser = new YrNoResultParser();
+                    Astrodata dataYr = parser.GetAstrodataByResult(resultYr);
+
+                    bool isSunrise = false;
+                    bool isSunset = false;
+                    DateTime sunrise = DateTime.MinValue;
+                    DateTime sunset = DateTime.MinValue;
+                    SunTimes.Instance.CalculateSunRiseSetTimes(lat, lon, date, ref sunrise, ref sunset, ref isSunrise,
+                        ref isSunset);
+
+                    bool areclosetoequal = this.AreCloseToEqual(dataYr.Data.Location.Sun.Rise, sunrise);
+                    Assert.IsTrue(areclosetoequal);
+
+                    bool areclosetoequal2 = this.AreCloseToEqual(dataYr.Data.Location.Sun.Set, sunset);
+                    Assert.IsTrue(areclosetoequal2);
+
+                    Thread.Sleep(300);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    i--; // Probably got a no rise result
+                }
+            }
+        }
+        
+        private bool AreCloseToEqual(DateTime date1, DateTime date2)
+        {
+            date1 = new DateTime(date1.Year, date1.Month, date2.Day, date1.Hour, date1.Minute, date1.Second); // Ensure same day, yrno sometimes send the next day
+            long ticksdiff = Math.Abs(date1.Ticks - date2.Ticks);
+            double secondsdiff = TimeSpan.FromTicks(ticksdiff).TotalSeconds;
+
+            return secondsdiff < 300; // Max 5 minutes diff
+        }
+
+        private double GetRandLat()
+        {
+            return this.GetRandomNumber(-85, 85);
+        }
+
+        private double GetRandLong()
+        {
+            return this.GetRandomNumber(-150, 150); // (-180, 180);
+        }
+
+        private double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            double randDouble = random.NextDouble() * (maximum - minimum) + minimum;
+
+            return randDouble;
         }
     }
 }
